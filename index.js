532b7f4d5390024cca76877aa1bbc7037dd05f60
hs37harsh <hs741999@gmail.com>
2024-09-16 11:15:50 +0530

    updation

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const user = require('./routes/user');
+const app = express();
+app.use(user);
+
+
+
+
+
+
+app.listen(3000,(err)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log('Server is running on port 3000');
+    }
+
+})
